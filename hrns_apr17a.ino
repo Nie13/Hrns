d7f4d18f04f0c9b0bbcc96c2879aebb74140508c
我家涅十三叔 <vvlll2flri@outlook.com>
2017-04-17 13:11:27 -0400

    first push

---
+++ hrns_apr17a.ino
@@ -0,0 +1,19 @@
+#include <ringbuffer.h>
+
+#include <lsm303.h>
+#include <lsm303reg.h>
+
+#include <Arduino.h>
+#include <libmaple/i2c.h>
+#include <math.h>
+
+int 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
